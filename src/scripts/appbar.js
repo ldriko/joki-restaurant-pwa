9b4f9aafdef4bbe8d797ad22f62ff48c25b8d1b7
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-06-04 22:48:48 +0700

    feat: add hero image & clean code

--- src/scripts/appbar.js
+++ src/scripts/appbar.js
@@ -1,7 +1,7 @@
 export const initializeNavBar = (id) => {
   const navbar = document.getElementById(id);
-  const menu = navbar.querySelector('.navbar__menu');
-  const burger = navbar.querySelector('.navbar__burger');
+  const menu = navbar.querySelector('.navbar-menu');
+  const burger = navbar.querySelector('.navbar-burger');
 
   burger.addEventListener('click', () => {
     menu.classList.toggle('active');
